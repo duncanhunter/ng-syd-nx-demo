4c5b6dad1d1136190cc1069845c8ebaa433e1360
Duncan Hunter <dh@Duncans-MacBook-Pro.local>
2019-12-10 19:48:40 +1100

    add header

--- apps/public-app/src/app/app.module.ts
+++ apps/public-app/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { SharedUiHeaderModule }from '@company-workspace/shared/ui/header';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, SharedUiHeaderModule],
   providers: [],
   bootstrap: [AppComponent]
 })
